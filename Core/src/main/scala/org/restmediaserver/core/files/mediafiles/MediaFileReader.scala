e27386a8f4fba0d7ed65439e610e21e5dc022817
Dan Pallas <git@danpallas.com>
2015-04-26 14:45:16 -0400

    add failed message to MediaFileReader

--- Core/src/main/scala/org/restmediaserver/core/files/mediafiles/MediaFileReader.scala
+++ Core/src/main/scala/org/restmediaserver/core/files/mediafiles/MediaFileReader.scala
@@ -1,17 +1,36 @@
 package org.restmediaserver.core.files.mediafiles
 
-import java.io.File
+import java.io.{File, IOException}
 
 import akka.actor.Actor
+import com.typesafe.scalalogging.LazyLogging
 
 /** When sent a File that is the path of an actual MediaFile, the file is read in from the file system and
-  * Option[MediaFile] is sent back.
+  * a MediaFile is sent back. If the file can not be read as a MediaFile then sends back Failed(file)
   * @author Dan Pallas
   * @since v1.0 on 4/25/15
  */
-class MediaFileReader extends Actor{
+class MediaFileReader extends Actor with LazyLogging{
+  import org.restmediaserver.core.files.mediafiles.MediaFileReader.Failed
   val TestSystem: String = "MetaDataTestSystem"
   override def receive: Receive = {
-    case path: File => sender ! MediaFile(path)
+    case path: File =>
+      try {
+        val result = MediaFile(path)
+        result match {
+          case Some(song) => sender ! song
+          case None => sender ! Failed(path)
+        }
+      } catch {
+        case e: IOException => {
+          sender ! Failed(path)
+          logger.error(s"Failed to load media file $path", e)
+        }
+        case e: Throwable => throw e
+      }
   }
+}
+object MediaFileReader{
+  /** path could not be read in as MediaFile */
+  case class Failed(path: File)
 }
--- Core/src/test/scala/org/restmediaserver/core/files/mediafiles/MediaFileReaderTest.scala
+++ Core/src/test/scala/org/restmediaserver/core/files/mediafiles/MediaFileReaderTest.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import akka.actor.{ActorRef, Props}
 import akka.pattern.ask
+import org.restmediaserver.core.files.mediafiles.MediaFileReader.Failed
 import org.restmediaserver.core.testfixtures.LibraryFixture.Library
 import org.restmediaserver.core.testsettings.AkkaTestSettings
 import org.scalatest.{Matchers, fixture}
@@ -27,34 +28,32 @@ class MediaFileReaderTest extends fixture.FunSuite with Matchers with AkkaTestSe
     }
   }
 
-  test("When MediaFileReader is sent a valid file that is a Song, a song should be sent back"){ reader =>
+  test("When MediaFileReader is sent a valid file that is a Song, a song should be sent back") { reader =>
     val songFile = Library.Music1.song3Mp3.path
     val f: Future[Any] = reader ? songFile
     val returnVal = Await.result(f, 5 seconds)
-    returnVal match {
-      case Some(song) => song.isInstanceOf[Song] shouldBe true
-      case _ => fail("Song was not sent back")
-    }
+    returnVal.isInstanceOf[Song] shouldBe true
   }
 
-  test("When MediaFileReader is is passed a file with a non song file extension it returns None"){ reader =>
+  test("When MediaFileReader is is passed a file with a non song file extension it returns Failed message with sent " +
+    "file"){
+    reader =>
     val badFile =  Library.Music1.songBadExtension.path
     val mediaFile = reader ? badFile
     val result = Await.result(mediaFile, 5 seconds)
     result match {
-      case None => true
-      case Some(_) => fail("something")
-      case _ => fail("something else")
+      case Failed(path) => path shouldBe badFile
+      case _ => fail("should have failed")
     }
   }
 
-  test("When MediaFileReader is sent a nonexistent file it should reply with None"){ reader =>
+  test("When MediaFileReader is sent a nonexistent file it should reply with Failed(path)"){ reader =>
     val songFile = new File("/nonExistentFile")
     val f = reader ?  songFile
     val result = Await.result(f, 5 seconds)
     result match {
-      case None => true
-      case _ => fail("result should be empty")
+      case Failed(path) => path shouldBe songFile
+      case _ => fail("result was not Failed(path)")
     }
   }
 }
