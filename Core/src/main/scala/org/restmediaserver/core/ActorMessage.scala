e2518caf0fb6b8f393b77f85a68444278d55f02d
Dan Pallas <git@danpallas.com>
2015-04-28 22:01:13 -0400

    rename MediaLibrary with correct capitalization. Add ActorMessage superclass

---
+++ Core/src/main/scala/org/restmediaserver/core/ActorMessage.scala
@@ -0,0 +1,8 @@
+package org.restmediaserver.core
+
+/**
+ * Created by Dan Pallas on 4/27/15.
+ */
+class ActorMessage {
+
+}
---
+++ Core/src/main/scala/org/restmediaserver/core/library/MediaLibrary.scala
@@ -0,0 +1,24 @@
+package org.restmediaserver.core.library
+
+import akka.actor.Actor
+import org.restmediaserver.core.ActorMessage
+import org.restmediaserver.core.files.mediafiles.Song
+
+import java.io.File
+
+/** Persistence service for media file library
+  * @author Dan Pallas
+  * @since v1.0 on 4/12/15.
+ */
+abstract class MediaLibrary extends Actor {
+
+}
+object MediaLibrary {
+  case class PutSongMsg(song: Song) extends ActorMessage
+  case class SuccessfulPut(path: File) extends ActorMessage
+  case class NotPutOlder(path: File) extends ActorMessage
+  case class PutException(path: File, ex: Exception) extends ActorMessage
+  case class GetSongMsg(song: Song) extends ActorMessage
+  case class GetLibraryFolderMsg(path: File) extends ActorMessage
+  case class PutFolderMsg(folder: File) extends ActorMessage
+}
